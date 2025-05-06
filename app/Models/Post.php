1794a6d4c693ebf8b590e18eda697f3fef494ae0
Taiki-Suzuki-12 <Taiki_Suzuki1729@outlook.jp>
2025-05-06 10:39:46 +0000

    toukou-sakujyo

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,14 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
+    use SoftDeletes;
+   
     protected $fillable = [
         'title',
         'body',
     ];
+    
     use HasFactory;
+
     public function getByLimit(int $limit_count = 3)
     {
         // updated_atで降順に並べたあと、limitで件数制限をかける
